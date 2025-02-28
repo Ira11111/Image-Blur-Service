47b57d3b9a22a1ce5f36a727070df6741c40b66f
Ira11111 <mirina-0208@yandex.ru>
2025-02-28 20:06:50 +0300

    models created

---
+++ application/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, Column, String, Boolean, ForeignKey, inspect
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+engine = create_engine("sqlite:///blur_application.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    email = Column(String, primary_key=True)
+    subscription = Column(Boolean, default=False)
+    orders = relationship("Order", back_populates="user")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    order_id = Column(String, primary_key=True)
+    directory = Column(String, unique=True, nullable=False)
+    user_email = Column(String, ForeignKey("users.email"), nullable=False)
+    user = relationship("User", back_populates="orders", cascade="all")
+
+
+def create_db():
+    inspector = inspect(engine)
+    if not inspector.has_table("users"):
+        Base.metadata.create_all(bind=engine)
